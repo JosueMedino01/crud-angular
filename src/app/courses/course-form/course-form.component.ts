f8c9d350942ce0cdd6cfc3887399c482b8096260
Josué Medino <josuemedino5@gmail.com>
2024-04-19 23:09:29 -0300

    feat: logical to back a initial page

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-course-form',
@@ -16,6 +18,7 @@ export class CourseFormComponent {
     private formBuilder: FormBuilder,
     private couserService: CoursesService,
     private _snackBar: MatSnackBar,
+    private location:Location
   ){
     this.formCourse = this.formBuilder.group({
       name: [null],
@@ -24,21 +27,35 @@ export class CourseFormComponent {
   }
 
   onSubmit(){
-    this.couserService.save(this.formCourse.value).subscribe(
-      data => console.log(data),
+    this.couserService.save(this.formCourse.value).subscribe({
+      next:(data:Course) => {
+      this.onSuccess();
+      },
 
-      error => this.onError(error),
-    );
+      error:(error:any) => {
+        this.onError(error)
+      },
+    });
   }
 
   onCancel(){
-
+    this.location.back();
   }
 
-  onError(error: any){
+  private onSuccess(){
+    this._snackBar.open(`Curso cadastrado com sucesso!`,"", {
+      verticalPosition: 'bottom',
+      horizontalPosition: 'left',
+      duration:5000
+    })
+
+    this.location.back();
+  }
+
+  private onError(error: any){
     this._snackBar.open(`Problema ao salvar curso. Erro ${error.status}`,"OK", {
       verticalPosition: 'bottom',
-      horizontalPosition: 'right',
+      horizontalPosition: 'left',
       duration:5000
     })
   }

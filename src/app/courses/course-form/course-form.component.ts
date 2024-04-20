e79c8102e8754f4ec524677ca391aca347bf5480
Josué Medino <josuemedino5@gmail.com>
2024-04-19 23:17:06 -0300

    refactor: modified the type forms

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
@@ -11,19 +11,18 @@ import { Course } from '../model/course';
   styleUrl: './course-form.component.scss'
 })
 export class CourseFormComponent {
-  formCourse!: FormGroup;
+  formCourse = this.formBuilder.group({
+    name: [''],
+    category: [''],
+  })
 
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private couserService: CoursesService,
     private _snackBar: MatSnackBar,
     private location:Location
   ){
-    this.formCourse = this.formBuilder.group({
-      name: [null],
-      category: [null],
-    })
   }
 
   onSubmit(){
